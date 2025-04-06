eeda1ff2e035cebf6aa0002c566063101cdd955d
kshhyun <tjgus8288508@gmail.com>
2025-04-06 21:51:23 +0900

    update gradle project packages

---
+++ artifinity/src/test/java/com/example/artifinity/ArtifinityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artifinity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifinityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
