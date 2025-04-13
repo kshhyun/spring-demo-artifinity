db2b2ca2ccf469ca4dfe2d7a60521334dce71a56
kshhyun <tjgus8288508@gmail.com>
2025-04-13 23:17:55 +0900

    feat: Add new fields and constructor to Notice entity

--- artifinity/src/main/java/com/example/artifinity/entity/Notice.java
+++ artifinity/src/main/java/com/example/artifinity/entity/Notice.java
@@ -1,4 +1,73 @@
 package com.example.artifinity.entity;
 
+import java.sql.Date;
+
 public class Notice {
+    private int noticeId;
+    private String noticeTitle;
+    private String noticeContent;
+    private Date noticeCreation;
+    private int userId;
+    private String userName;
+
+
+    public Notice() {
+
+    }
+
+
+    public Notice(String noticeTitle, String noticeContent, Date noticeCreation, int userId) {
+        this.noticeTitle = noticeTitle;
+        this.noticeContent = noticeContent;
+        this.noticeCreation = noticeCreation;
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getNoticeId() {
+        return noticeId;
+    }
+
+    public void setNoticeId(int noticeId) {
+        this.noticeId = noticeId;
+    }
+
+    public String getNoticeTitle() {
+        return noticeTitle;
+    }
+
+    public void setNoticeTitle(String noticeTitle) {
+        this.noticeTitle = noticeTitle;
+    }
+
+    public String getNoticeContent() {
+        return noticeContent;
+    }
+
+    public void setNoticeContent(String noticeContent) {
+        this.noticeContent = noticeContent;
+    }
+
+    public java.sql.Date getNoticeCreation() {
+        return noticeCreation;
+    }
+
+    public void setNoticeCreation(java.sql.Date noticeCreation) {
+        this.noticeCreation = noticeCreation;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
 }
